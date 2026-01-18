03c144a8a6736236383666bb924b79a855ad07a0
Wagner Sanches <wagnersanches5@hotmail.com>
2026-01-18 15:56:19 -0300

    Adicionando metodos de criptografia

---
+++ internal/crypto/argon2.go
@@ -0,0 +1,23 @@
+package crypto
+
+import (
+	"golang.org/x/crypto/argon2"
+)
+
+const (
+	Memory 		= 1024 * 64 // 64 mb
+	Iterations 	= 3
+	Parallelism = 2
+	KeyLength 	= 32
+)
+
+func DeriveKey(password string, salt []byte) []byte {
+	return argon2.IDKey(
+		[]byte(password),
+		salt,
+		Iterations,
+		Memory,
+		Parallelism,
+		KeyLength
+	)
+}
